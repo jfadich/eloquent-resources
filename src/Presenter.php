fc1862bb61712019afec5655eb0dd623a2bed5b6
John Fadich <jofad4@gmail.com>
2016-10-27 00:22:34 -0700

    add presenters

---
+++ src/Presenter.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace jfadich\JsonResponder;
+
+use jfadich\JsonResponder\Contracts\Presentable;
+
+/**
+ * Base presenter class to manage calling property presenter methods on the model
+ */
+class Presenter
+{
+    /**
+     * Model to present data from
+     *
+     * @var Presentable
+     */
+    protected $model;
+
+    /**
+     * Default format for dates
+     *
+     * @var string
+     */
+    protected $dateFormat = 'm/d/Y';
+
+    /**
+     * @param Presentable $model
+     */
+    function __construct(Presentable $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * When a property is referenced search for the associated method on the presenter
+     * If it doesn't exist defer to the model
+     *
+     * @param string $property , [...]
+     * @param array $options
+     * @return mixed
+     */
+    public function present($property, ...$options)
+    {
+        if (method_exists($this, $property)) {
+            return call_user_func_array([$this, $property], $options);
+        }
+
+        if (in_array( $property, $this->model->getDates() )) {
+            array_unshift($property,$options);
+            return call_user_func_array([$this, 'presentDate'], $options );
+        }
+
+        return $this->model->{$property};
+    }
+
+    /**
+     * Present the date on the model in the given format
+     *
+     * @param $dateField
+     * @param null $format
+     * @return mixed
+     */
+    public function presentDate($dateField, $format = null, $default = null)
+    {
+        if ( $format === null )
+            $format = $this->dateFormat;
+
+        if (($date = $this->model->{$dateField}) === null || $date->timestamp <= 0)
+            return $default;
+
+        if($format === 'timestamp')
+            return $date->timestamp;
+
+        if($format === 'diff' || $format === 'diffForHumans')
+            return $date->diffForHumans();
+
+        return $date->format( $format );
+    }
+
+    /**
+     * If the model ID is a UUID, convert the binary representation into something that can be displayed
+     *
+     * @return mixed
+     */
+    public function id()
+    {
+        if(!ctype_print($this->model->id))
+            return bin2hex($this->model->id);
+
+        return $this->model->id;
+    }
+
+    /**
+     * Magic shortcut to present($property)
+     *
+     * @param string $property
+     * @return mixed
+     */
+    public function __get($property)
+    {
+        return $this->present($property);
+    }
+
+    /**
+     * If a property is referenced and there is no presenter method, check if the property is a date
+     * if so sent it to presentDate()
+     *
+     * @param $method
+     * @param $arguments
+     * @return mixed
+     */
+    public function __call($method, $arguments)
+    {
+        array_unshift($arguments, $method);
+
+        if (in_array($method, $this->model->getDates())) {
+            return call_user_func_array([$this, 'presentDate'], $arguments);
+        }
+
+        return call_user_func_array([$this, 'present'], $arguments);
+    }
+}
---
+++ src/Traits/Presentable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace jfadich\JsonResponder\Traits;
+
+use jfadich\JsonResponder\Presenter;
+
+/**
+ * Trait to easily add a presenter to a model
+ */
+trait Presentable
+{
+    /**
+     * View presenter instance
+     *
+     * @var mixed
+     */
+    protected $presenter;
+
+    /**
+     * If a property is provided, presenter will parse the property on the model
+     * using any additional arguments as options.
+     * Otherwise return the presenter.
+     *
+     * @param $property
+     * @param array $options
+     * @return mixed
+     */
+    public function present($property = null, ...$options)
+    {
+        $presenter = $this->getPresenter();
+
+        if($property !== null)
+            return call_user_func_array([$presenter, $property], $options );
+
+        return $presenter;
+    }
+
+    /**
+     * Get an instance of the presenter object for this model.
+     *
+     * @return mixed|string
+     */
+    public function getPresenter()
+    {
+        if ($this->presenter === null)
+            $this->presenter = $this->resolvePresenterName();
+
+        if (is_string($this->presenter)) {
+            if (!class_exists($this->presenter))
+                $this->presenter = Presenter::class;
+
+            $this->presenter = new $this->presenter($this);
+        }
+
+        return $this->presenter;
+    }
+
+    protected function resolvePresenterName()
+    {
+        $modelNamespace =  property_exists($this, 'modelNamespace') ? $this->modelNamespace : 'App';
+        $presenterNamespace = property_exists($this, 'presenterNamespace') ? $this->presenterNamespace : 'App\\Presenters';
+
+        return str_replace($modelNamespace, $presenterNamespace, get_class($this)) . 'Presenter';
+    }
+}
