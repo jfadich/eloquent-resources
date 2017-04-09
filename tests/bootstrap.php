92701dfe3f1e0fc1ac47cb5fa4c2aa36d7f70139
John Fadich <jofad4@gmail.com>
2017-04-09 16:26:51 -0700

    add include*  transformer test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,38 +1,76 @@
 <?php
 
 namespace {
+
+    use jfadich\JsonResponder\TransformationManager;
+
     require __DIR__ . '/../vendor/autoload.php';
 
-    function config($key = null) {
-        $config = include __DIR__ . '/../config/config.php';
-        $config = ['transformers' => $config];
+    if( ! function_exists('config')) {
+        function config($key = null) {
+            $config = include __DIR__ . '/../config/config.php';
+            $config = ['transformers' => $config];
 
-        if($key !== null)
-            return array_get($config, $key);
+            if($key !== null)
+                return array_get($config, $key);
             //return $config->get($key);
 
-        return $config;
+            return $config;
+        }
     }
+
+    if( ! function_exists('app')) {
+        function app($make) {
+            static $manager;
+
+            if($make === TransformationManager::class) {
+                if ($manager === null) {
+                    $config = config('transformers.namespaces');
+                    $manager = new TransformationManager($config['models'], $config['transformers'], $config['presenters']);
+                }
+
+                return $manager;
+            }
+
+            return null;
+        }
+    }
+
 
 }
 
 namespace App {
+
+    use App\Nested\NestedModel;
+    use Illuminate\Database\Eloquent\Model;
     use jfadich\JsonResponder\Contracts\Transformable as TransformableContract;
     use jfadich\JsonResponder\Traits\Transformable;
 
 
-    class TestModel implements TransformableContract{
+    class TestModel extends Model implements TransformableContract{
         use Transformable;
+
+        public function nestedModel()
+        {
+            return $this->hasOne(NestedModel::class);
+        }
     }
 
 }
 
 namespace App\Nested {
+
+    use Illuminate\Database\Eloquent\Model;
     use jfadich\JsonResponder\Contracts\Transformable as TransformableContract;
     use jfadich\JsonResponder\Traits\Transformable;
 
-    class NestedModel implements TransformableContract{
+    class NestedModel extends Model implements TransformableContract{
         use Transformable;
+
+        public function getConnection()
+        {
+            return new \Illuminate\Database\Connection(null);
+        }
     }
 }
 
@@ -40,6 +78,7 @@ namespace App\Transformers {
     use jfadich\JsonResponder\Transformer;
 
     class TestModelTransformer extends Transformer {
+        protected $availableIncludes = ['nestedModel'];
     }
 }
 
