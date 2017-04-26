7d8fe3a0df4ccb4986089969fbf6f25f125d5683
John Fadich <jofad4@gmail.com>
2017-04-25 22:58:25 -0700

    update base controller to not boot responds with resources

--- src/Http/ResourceController.php
+++ src/Http/ResourceController.php
@@ -2,24 +2,10 @@
 
 namespace jfadich\EloquentResources\Http;
 
-use Illuminate\Routing\Controller as BaseController;
 use jfadich\EloquentResources\Traits\RespondsWithResources;
-use League\Fractal\Manager as Fractal;
-use Illuminate\Http\Request;
-
+use Illuminate\Routing\Controller as BaseController;
 
 class ResourceController extends BaseController
 {
     use RespondsWithResources;
-
-    /**
-     * Boot the RespondsWithResources trait
-     *
-     * @param Fractal $fractal
-     * @param Request $request
-     */
-    public function __construct(Fractal $fractal, Request $request)
-    {
-        $this->bootRespondsWithResources($fractal, $request);
-    }
 }
